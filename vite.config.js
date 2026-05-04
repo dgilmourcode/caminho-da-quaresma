fa6fb740e626f5b382ecd211a44b24eb5dd2b903
David Gilmour <davidgilmour144@gmail.com>
2026-05-04 05:51:19 -0300

    feat: Melhoria no menu mobile

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,23 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/caminho-da-quaresma/',
   root: '.',
   publicDir: 'public',
+
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    emptyOutDir: true,
     rollupOptions: {
-      input: { main: resolve(__dirname, 'index.html') },
-    },
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   },
-  server: { port: 3000, open: true },
+
+  server: {
+    port: 3000,
+    open: true
+  }
 })
